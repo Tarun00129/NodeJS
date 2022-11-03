777d691405c827c802a8745676350bee9f88a8b6
Tarun00129 <tarungovindpuri@gmail.com>
2022-11-03 12:33:02 +0530

    new Async

--- fs_asyc/index.js
+++ fs_asyc/index.js
@@ -1,4 +1,21 @@
 const fs = require("fs")
-fs.writeFile("myFile.txt","hiiiiiiiii we are working in Async FS ",(err)=>{
-    console.log({err});
+// fs.writeFile("myFile.txt","hiiiiiiiii we are working in Async FS ",(err)=>{
+//     console.log('file is created');
+//     console.log(err);
+// // })
+// fs.appendFile('myFile.txt'," OP",
+// (err)=>{
+//     console.log("Task Completed")
+// })
+
+// fs.readFile('myFile.txt','utf-8',(err,data) =>{
+//     console.log(data);
+// })
+
+// fs.appendFile('myFile.txt'," OP",
+// (err)=>{
+//     console.log("Task Completed")
+// })
+fs.readFile('myFile.txt','utf-8',(err,data)=>{
+    console.log(data);
 })
