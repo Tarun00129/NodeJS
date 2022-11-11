9f635894bfe8b30cab335efccea92af956cc5b85
Tarun00129 <tarungovindpuri@gmail.com>
2022-11-11 17:47:54 +0530

    QApp comes to Existence (Starts)

--- Events/Index.js
+++ Events/Index.js
@@ -1,36 +1,16 @@
 const EventEmmiter = require("events")
 const event = new EventEmmiter();
 
-event.on("wtf1",()=>{
-    console.log("what is your name ");
+
+
+event.on("wtf2",(sc,msg)=>{
+    console.log(`status code is ${sc} amd msg is ${msg} !!!!!`);
 })
-event.on("wtf2",()=>{
-    console.log("My name is Tarun");
+
+event.on("tarun",(sc,msg)=>{
+    console.log(`The status code is ${sc} and the ${msg} *****`)
 })
-event.on("wtf2",()=>{
-    console.log("what is your age");
-})
-event.on("wtf4",()=>{
-    console.log("I am 22 year old");
-})
-console.log("*************************************************************************");
-event.emit("wtf1")
-event.emit("wtf2")
-event.emit("wtf3")
-event.emit("wtf4")
-console.log("*************************************************************************");
-event.emit("wtf1")
-event.emit("wtf2")
-event.emit("wtf1")
-event.emit("wtf2")
-console.log("*************************************************************************");
-event.emit("wtf1")
-event.emit("wtf3")
-event.emit("wtf4")
-event.emit("wtf2")
-console.log("*************************************************************************");
-event.emit("wtf4")
-event.emit("wtf3")
-event.emit("wtf2")
-event.emit("wtf1")
-console.log("*************************************************************************");
+
+event.emit("wtf2",200,'ok')
+
+event.emit('tarun',304,'redirecting it self')
