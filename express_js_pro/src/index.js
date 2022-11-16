5198a6a672d95b83f2129680e2679b1fdea922fe
Tarun00129 <tarungovindpuri@gmail.com>
2022-11-16 11:52:14 +0530

    adding a commit

--- express_js_pro/src/index.js
+++ express_js_pro/src/index.js
@@ -3,10 +3,20 @@ const app = express();
 var path = require("path");
 
 const staticPath = path.join(__dirname,"../public")
+const viewsPath = path.join(__dirname,"../views/")
 
-console.log(path.join(__dirname,"../public")    );
+// console.log(path.join(__dirname,"../public"));
 
-app.use(express.static(staticPath));
+app.set("views",viewsPath)
+
+
+app.get("",(req,res)=>{
+    res.render("index.hbs")
+})
+
+// app.use(express.static(staticPath));
+
+
 
 app.get("/",(req,res)=>{
     res.send("hello, I am expressJS ");
